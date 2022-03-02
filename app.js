375982d115facce6a22b1dab0a2e65bcadc78d17
RomanVelnar <roman.velnar@gmail.com>
2022-03-02 19:42:59 +0000

    working RPS code, having issues with running JS after intro page

---
+++ app.js
@@ -0,0 +1,80 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTIONS = [
+    {
+        name: 'rock', 
+        emoji: '✊',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper', 
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors', 
+        emoji: '✌️',
+        beats: 'paper'
+    }
+]
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+    })
+})
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (computerWinner) incrementScore(computerScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
+
+// Instructions container
+const playButton = document.querySelector('[data-game-button]')
+const intro = document.querySelector('[data-instructions]')
+const game = document.querySelector('[data-container]')
+
+
+playButton.addEventListener('click', () => {
+    intro.innerHTML = game.innerHTML
+})
+
+
+
+
+// function toggleGame() {
+//     const gameInstructions = document.getElementsByClassName('instructions')
+//     const displayInstructions = gameInstructions.style.display
+// }
